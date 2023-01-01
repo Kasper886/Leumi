aa02389abb2a26d4a4304195312cdacd5f14160f
Kasper886 <aleksbulgakov@gmail.com>
2023-01-01 11:32:30 +0200

    ECR name was changed

--- EKS-Cluster/terraform/ecr.tf
+++ EKS-Cluster/terraform/ecr.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "guestbook-go" {
+resource "aws_ecr_repository" "PythonApp" {
   name                 = var.repo-name
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
