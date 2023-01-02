4c9fc43e048075939d0aab76844a0d6d5719287b
Kasper886 <aleksbulgakov@gmail.com>
2023-01-02 17:43:31 +0200

    update

--- 2.EC2/WebServer.tf
+++ 2.EC2/WebServer.tf
@@ -26,35 +26,3 @@ resource "aws_instance" "my_webserver" {
     create_before_destroy = true
   }
 }
-
-resource "aws_security_group" "my_webserver" {
-  name        = "WebServer Security Group"
-  description = "Web Security Group"
-  vpc_id      = aws_vpc.test-spoke-vpc.id
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name  = "Web Server SecurityGroup"
-    Owner = "Alex Largman"
-  }
-}
