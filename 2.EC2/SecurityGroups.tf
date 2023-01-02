9c9a54dfb2a60a0d2c5a409e5351cef53b905294
Kasper886 <aleksbulgakov@gmail.com>
2023-01-02 16:34:44 +0200

    SG changed

---
+++ 2.EC2/SecurityGroups.tf
@@ -0,0 +1,31 @@
+resource "aws_security_group" "my_webserver" {
+  name        = "WebServer Security Group"
+  description = "Web Security Group"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name  = "Web Server SecurityGroup"
+    Owner = "Alex Largman"
+  }
+}
